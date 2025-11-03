e05d7064a2e08ec2065c237aec10a353585fea5e
pavan-0307 <gowdampavan976@gmail.com>
2025-11-03 11:19:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// JavaScript for the mobile navigation menu
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    // Toggle Nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+
+    // Close nav when a link is clicked
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (nav.classList.contains('nav-active')) {
+                nav.classList.remove('nav-active');
+                burger.classList.remove('toggle');
+            }
+        });
+    });
+}
+
+// Call the function to activate the mobile nav
+navSlide();
